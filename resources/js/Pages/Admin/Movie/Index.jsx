0e678625e380080834aa2e1386c1be447789c68d
Hairul Ardhan Nawazaki <hanawazaki002@gmail.com>
2023-06-28 19:10:49 +0700

    feat: admin update

--- resources/js/Pages/Admin/Movie/Index.jsx
+++ resources/js/Pages/Admin/Movie/Index.jsx
@@ -4,15 +4,56 @@ import React from 'react'
 import { Link } from '@inertiajs/inertia-react'
 import FlashMessage from '@/Components/FlashMessage'
 
-const Index = ({ auth, flashMessage }) => {
+const Index = ({ auth, flashMessage, movies }) => {
   return (
     <Authenticated auth={auth}>
       <Link href={route('admin.dashboard.movie.create')}>
         <Button type='button' className="w-40 mb-8">Insert new movie</Button>
       </Link>
       {
-        flashMessage?.message && <FlashMessage message={flashMessage.message} />
+        flashMessage?.message && (<FlashMessage message={flashMessage.message} />)
       }
+
+      <table className='table-fixed w-full text-center'>
+        <thead>
+          <tr>
+            <th>Image</th>
+            <th>Name</th>
+            <th>Category</th>
+            <th>Rating</th>
+            <th colSpan={2}>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            movies.map((movie, index) => (
+              <tr key={index}>
+                <td>
+                  <img src={`/storage/${movie.thumbnail}`} className='w-32 rounded-md' alt="movie-img" />
+                </td>
+                <td>
+                  {movie.name}
+                </td>
+                <td>
+                  {movie.category}
+                </td>
+                <td>
+                  {movie.rating.toFixed(1)}
+                </td>
+                <td>
+                  <Link href={route('admin.dashboard.movie.edit', movie.id)}>
+                    <Button type='button' variant='warning'>Ubah</Button>
+                  </Link>
+                </td>
+                <td>
+                  <Button type='button' variant='danger'>Hapus</Button>
+                </td>
+              </tr>
+            ))
+          }
+        </tbody>
+
+      </table>
     </Authenticated>
   )
 }
